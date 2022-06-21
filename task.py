6156f7419f112e7cb16244b96206b6cf5a7704b3
Tomek Dyl <tomekdyl@MacBook-Pro.local>
2022-06-21 19:02:37 +0200

    initialize

---
+++ task.py
@@ -0,0 +1,3 @@
+import json
+
+data = { "foo": ["bar", "baz"], "orange": ["banana", "mango"], "bar": ["qux", "quux"], "monkey": ["cow", "parrot"], "banana": ["mango", "monkey"], "baz": ["baz"], "quux": ["bar", "banana"], "cow": ["orange"] }
