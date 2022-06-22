f141155f8418a1fe8e7e4f66d987a991dcb65ed9
Tomasz Dyl <tomasz.dyl@krontab.eu>
2022-06-22 16:00:44 +0200

    added new parameters to function: cycles, counter replaced by start list, removing the value from the list in line 56

--- task.py
+++ task.py
@@ -1,4 +1,7 @@
 
+from queue import Empty
+
+
 data = { "foo": ["bar", "baz"], 
 "orange": ["banana", "mango"], 
 "bar": ["qux", "quux"], 
@@ -18,39 +21,46 @@ def remove_head(head,tail,cycle):
         return cycle
 
 
-def walk_cycle(v,data,values,counter = 0,cycle = []):
+def walk_cycle(v,data,values,start,cycle = [],cycles = []):
 
         if v in data.keys() and v in cycle:
             
             cycle.append(v)
             #check if cycle
-            if cycle[0] == cycle[len(cycle)-1]:
-                return cycle
-            else:
-                head = cycle[0]
-                tail = cycle[len(cycle)-1]
+            head = cycle[0]
+            tail = cycle[len(cycle)-1]
+            if head !=tail:
                 remove_head(head,tail,cycle)
-                        
-            return cycle
+            start.remove(v)
+            if len(start) > 0 :
+                #checking next value from the list for cycle
+                #new cycle
+                cycles.append(cycle)
+                walk_cycle(start[0],data,values,start,cycle=[],cycles=cycles) 
+            #if no more values in start list, return cycles, proceed with next key in json
+            return cycles
 
         elif v in data.keys():        
             values = data[v]
-            #add to cycle, cycle could exist
+            #add to cycle, cycle migh exist, value in keys
             cycle.append(v)
             value = data[v][0]
-            #remove to return to previous
-            data[v][0] = None
+            
+            #data[v][0] = None
+            #data[v].remove(value)
             #print(cycle)
            
-            walk_cycle(value,data,values,counter,cycle)
+            walk_cycle(value,data,values,start,cycle,cycles)
         else:
-            counter+=1
-            if counter < len(values):
+            #if not in keys, no cycle
+            values.remove(v)
+            #counter+=1
+            #if counter < len(values):
 
-                walk_cycle(values[counter],data,values,counter,cycle)
+            walk_cycle(values[0],data,values,start,cycle,cycles)
 
 
-        return cycle
+        return cycles
 
 def filter_cycle(cycle):
     if cycle[0] == cycle[len(cycle)-1]:
@@ -59,8 +69,9 @@ def filter_cycle(cycle):
 
 if __name__=='__main__':
     minimal_cycles = []
+
     for k in data.keys():
-        cycle = walk_cycle(k,data,data[k],counter = 0,cycle = [])
+        cycles = walk_cycle(k,data,data[k],data[k],cycle = [],cycles = [])
         print(cycle)
         cycle = filter_cycle(cycle)
         minimal_cycles.append(cycle)
