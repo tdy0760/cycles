f48c57f5b4d9e0a3db09ea4273b5b9092db62d79
Tomasz Dyl <tomasz.dyl@krontab.eu>
2022-06-24 12:09:04 +0200

    added new function to format cycle

--- task.py
+++ task.py
@@ -1,4 +1,7 @@
 
+from tkinter.ttk import Separator
+
+
 data = { "foo": ["bar", "baz"], 
 "orange": ["banana", "mango"], 
 "bar": ["qux", "quux"], 
@@ -10,6 +13,9 @@ data = { "foo": ["bar", "baz"],
  }
 
 def remove_head(head,tail,cycle):
+    '''
+        if not cycle returns empty list
+    '''
     if head != tail:
         cycle.remove(head)
         head=cycle[0]
@@ -19,75 +25,96 @@ def remove_head(head,tail,cycle):
 
 
 def walk_cycle(v,data,values,start,cycle = [],cycles = []):
+    '''
+        v - current search value, cycle begining value
+        data - json data
+        values - list assigned to the key
+        start - cycle begining list
+        cycle - accumulator for cycle elements
+        cycles - accumulator for cycles
+    '''
 
-        if v in data.keys() and v in cycle:
-            
-            cycle.append(v)
-            #check if cycle
-            head = cycle[0]
-            tail = cycle[len(cycle)-1]
-            if head !=tail:
-                remove_head(head,tail,cycle)
-            #removing v from start if in the list
-            #if not in the list that means v is somwhere at the end of the cycle
-            if v in start:
-                start.remove(v)
-            else:
-                #remove v from the end, we will not be chasing twice the same
-                try:
-                    values.remove(v)
-                except:
-                    pass
+    if v in data.keys() and v in cycle:
+        
+        cycle.append(v)
+        #check if cycle
+        head = cycle[0]
+        tail = cycle[len(cycle)-1]
+        if head !=tail:
+            remove_head(head,tail,cycle)
 
-            cycles.append(cycle)
-            
-            #if something left in the list go ahead with searching for new cycle
-            if len(start) > 0 :
-                walk_cycle(start[0],data,values,start,cycle=[],cycles=cycles) 
-            #if no more values in start list, return cycles, proceed with next key in json
-            return cycles
-
-        elif v in data.keys():        
-            values = data[v]
-            #add to cycle, cycle migh exist, value in keys
-            cycle.append(v)
-            value = None
-            if len(data[v]) > 0:
-                value = data[v][0]
-
-            #when we add value to the cycle we remove it from the start and data
-            if v in start:
-                start.remove(v)
-            
-            try:
-                if v in data[value]:
-                    data[value].remove(v)
-            except KeyError:
-                pass
-           
-            walk_cycle(value,data,values,start,cycle,cycles)
+        #removing v from start if in the list
+        #if not in the list that means v is somwhere at the end of the cycle
+        if v in start:
+            start.remove(v)
         else:
-            #if not in keys, no cycle, remove from the list of the values and from the start
-            if v in values:
+            #remove v from the end, we will not be chasing twice the same
+            try:
                 values.remove(v)
+            except:
+                pass
 
-            if v in start:
-                start.remove(v)
-            
-            if len(values) > 0:
-                walk_cycle(values[0],data,values,start,cycle,cycles)
-
-
+        cycles.append(cycle)
+        
+        #if something left in the list go ahead with searching for new cycle
+        if len(start) > 0 :
+            walk_cycle(start[0],data,values,start,cycle=[],cycles=cycles) 
+        #if no more values in start list, return cycles, proceed with next key in json
         return cycles
 
-def test_results():
+    elif v in data.keys():        
+        values = data[v]
+        #add to cycle, cycle migh exist, value in keys
+        cycle.append(v)
+        value = None
+        if len(data[v]) > 0:
+            value = data[v][0]
+
+        #when we add value to the cycle we remove it from the start and data
+        if v in start:
+            start.remove(v)
+        
+        try:
+            if v in data[value]:
+                data[value].remove(v)
+        except KeyError:
+            pass
+        
+        walk_cycle(value,data,values,start,cycle,cycles)
+    else:
+        #if not in keys, no cycle, remove from the list of the values and from the start of the cycle
+        if v in values:
+            values.remove(v)
+
+        if v in start:
+            start.remove(v)
+        
+        if len(values) > 0:
+            walk_cycle(values[0],data,values,start,cycle,cycles)
+
+
+    return cycles
+
+def test_results(formatted_cycle):
     cycles = ['bar -> quux -> bar',
             'baz -> baz',
             'orange -> banana -> monkey -> cow -> orange']
 
-def print_cycle(cycle):
     
+    assert formatted_cycle in cycles
 
+def format_cycle(cycle):
+    i = 0
+    s = ''
+    separator = " -> "
+    for e in cycle:
+        if i % 2 == 0:
+            s+=separator
+            s+=e
+        else:
+            s+=e
+            i+=1
+    return s
 
 
 if __name__=='__main__':
@@ -98,9 +125,13 @@ if __name__=='__main__':
         #print(cycles)
         #cycle = filter_cycle(cycle)
         minimal_cycles+=cycles
-
-
-
+    
     for c in minimal_cycles:
-        print(c)
+        formatted_cycle = format_cycle(c)
+        test_results(formatted_cycle)
+
+    #for c in minimal_cycles:
+    #    print(*c,sep=" -> ")
+    
+        
 
